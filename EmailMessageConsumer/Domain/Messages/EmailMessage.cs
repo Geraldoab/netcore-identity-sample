3f00ab17b1f704c21d4a6a4f61fdcb5401914153
Geraldo Aparecido Borges <geraldo.estudos.trabalho@gmail.com>
2024-01-14 16:27:11 -0300

    Added a hosted service to consume messages from RabbitMq

---
+++ EmailMessageConsumer/Domain/Messages/EmailMessage.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace EmailMessageConsumer.Domain.Messages
+{
+    public class EmailMessage
+    {
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("-----------------New message found----------------");
+            sb.AppendLine($"From: {From}");
+            sb.AppendLine($"To: {To}");
+            sb.AppendLine($"Subject: {Subject}");
+            sb.AppendLine($"Body: {Body}");
+            sb.AppendLine("--------------------------------------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ EmailMessageConsumer/EmailMessageSubscriber.cs
@@ -0,0 +1,50 @@
+﻿using EmailMessageConsumer.Domain.Messages;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+namespace EmailMessageConsumer
+{
+    public class EmailMessageSubscriber : BackgroundService
+    {
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private const string _queue = "userapi-email-service-queue";
+
+        public EmailMessageSubscriber()
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = Environment.GetEnvironmentVariable("RABBIT_MQ_URL")
+            };
+
+            _connection = connectionFactory.CreateConnection("userapi-provider-name");
+
+            _channel = _connection.CreateModel();
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var consumer = new EventingBasicConsumer(_channel);
+
+            consumer.Received += (sender, eventArgs) =>
+            {
+                var contentArray = eventArgs.Body.ToArray();
+                var contentString = Encoding.UTF8.GetString(contentArray);
+                var emailMessage = JsonSerializer.Deserialize<EmailMessage>(contentString);
+
+                if(emailMessage != null)
+                {
+                    Console.WriteLine(emailMessage.ToString());
+                }
+
+                _channel.BasicAck(eventArgs.DeliveryTag, false);
+            };
+
+            _channel.BasicConsume(_queue, false, consumer);
+
+            return Task.CompletedTask;
+        }
+    }
+}
